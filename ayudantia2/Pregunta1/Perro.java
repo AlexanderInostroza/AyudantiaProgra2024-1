10e07c958d85febb6c2caf1a7e4d5661d8ae8a3d
Alexander Inostroza <37822286+AlexanderInostroza@users.noreply.github.com>
2024-04-09 18:22:23 -0400

    Ayudantía 2 added

---
+++ ayudantia2/Pregunta1/Perro.java
@@ -0,0 +1,23 @@
+package Pregunta1;
+
+public class Perro {
+
+    // Constructor vacío, puede settear valores genéricos como nombre = ""
+    public Perro(){}
+
+    // Constructor
+    public Perro(String nombre, String raza, String dueño, int edad){}
+
+    // métodos setters
+    public void setNombre(String nombre){}
+    public void setRaza(String raza){}
+    public void setDueno(String dueño){}
+    public void setEdad(int edad){}
+    
+    // métodos getters
+    public String getNombre(){return "";}
+    public String getRaza(){return "";}
+    public String getDueno(){return "";}
+    public int getEdad(){return 0;}
+
+}
---
+++ ayudantia2/Pregunta2/CalculadoraDePiscinas.java
@@ -0,0 +1,15 @@
+package Pregunta2;
+
+public class CalculadoraDePiscinas {
+    // Constructor vacío
+    public CalculadoraDePiscinas(){}
+
+    // Constructor con dimensiones
+    public CalculadoraDePiscinas(int x, int y, int z){}
+
+    // Precio bajo la cotización de la empresa A
+    public int precioEmpresaA(){ return 0;}
+
+    // Precio bajo la cotización de la empresa B
+    public int precioEmpresaB(){ return 0;}
+}
