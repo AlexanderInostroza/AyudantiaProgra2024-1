019b3b277d5003d60652d9f69da9e3b64f5fc2d0
Alexander Inostroza <37822286+AlexanderInostroza@users.noreply.github.com>
2024-04-19 01:58:12 -0400

    Delete ayudantia3/Pregunta1 directory

--- ayudantia3/Pregunta1/Articulo.java
+++
@@ -1,9 +0,0 @@
-package Pregunta1;
-
-public interface Articulo {
-    public int getPrecio();
-    public int getStock();
-    public String getCategoria();
-    public String getTipo();
-    public String getNombre();
-}
